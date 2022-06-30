987b554d7ae1289841da7493d6a07c7ff1bb3738
Terer Mike <terermike3@gmail.com>
2022-06-30 16:28:45 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(void)
+/**
+ * main - Main function
+ * @i: First operand
+ * @f: Second operand
+ * @c: Third operand
+ * @l: Fourth operand
+ * @b: Fifth operand
+ *
+ * Return: 0 if it is a success
+ */
+{
+	int i;
+	float f;
+	char c;
+	long int l;
+	long long int b;
+
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int);
+	printf("Size of a float: %i byte(s)", sizeof(float));
+	return (0);
+}
