2451071cd01a682406ba22335d9762a1f058fd26
Terer Mike <terermike3@gmail.com>
2022-06-30 15:21:57 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - is the entry poin
+ *
+ * Return: Always 0 (Success)
+ */
+
+  int main(void)
+  {
+  puts("\"Programming is like building a multilingual puzzle);
+  return (0);
+  }
+
