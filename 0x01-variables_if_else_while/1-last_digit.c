82625e558a8e556b58dc1c532196bf59c815c2e2
Terer Mike <terermike3@gmail.com>
2022-07-01 11:03:08 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - This the main function which runs the program
+ *
+ *Return: Returns zero if it runs successfully
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d %s \n", n, is, );
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ *Return: Returns 0 if it is a success
+ */
+int main()
+{
+	char ch = 'a';
+	while (ch <= 'z')
+	{
+	putchar(ch);
+	ch++;
+	}
+	putchar(10);
+	return (0);
+}
