9703e0eca7a4424e4dfe512c53e7c35f45b8af26
Terer Mike <terermike3@gmail.com>
2022-07-01 11:13:43 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  * main - program runs in main
+  *
+  * Return: returns  0 when successful
+  */
+
+int main(void)
+
+{
+
+	int ch;
+	int uc;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(ch);
+	}
+	for (uc = 'A'; uc <= 'Z'; uc++)
+	{
+	putchar(uc);
+	}
+	putchar(10);
+	return (0);
+}
