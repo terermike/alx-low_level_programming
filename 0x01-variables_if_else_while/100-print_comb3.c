37654103ecc9619df3ae15de31437ee7c37388bd
Terer Mike <terermike3@gmail.com>
2022-07-03 14:57:38 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,83 @@
-#include <stdio.h>
+nclude <stdio.h>
+
 /**
- * main -main function
- *
- * Return: Always 0 Success
- */
+
+   * main - Entry point
+
+    *
+
+     * Return: Always 0 (Success/completed)
+
+      */
 
 int main(void)
-{
-	int i;
-	int j;
 
-	for (i = 48; i <= 57; i++)
+
+
+{
+
+	int number_left;
+
+	int number_right;
+
+
+
+
+
+	for (number_left = 48; number_right <= 78; number_left++)
+
 	{
-	for (j = 48; j <= 57; j++)
-	{
-	if (j > i)
-	{
-	putchar(i);
-	putchar(j);
-	if (i != 56)
-	{
-	putchar(',');
-	putchar(' ');
+
+		for (number_right = number_left + 1 ; number_right <= 78; number_right++)
+
+		{
+
+
+
+			putchar(number_left);
+
+			putchar (number_right);
+
+
+
+
+
+			if ((number_left == 56) && (number_right == 78))
+
+			{
+
+				break;
+
+			}
+
+
+
+			putchar(',');
+
+			putchar (' ');
+
+
+
+		}
+
+		  
+
+
+
 	}
-	}
-	}
+
+
+
 	putchar('\n');
+
+
+
+
+
 	return (0);
-	}	
+
+
+
+
+
 }
