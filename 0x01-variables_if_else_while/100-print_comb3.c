9ad1dc234979853c426cdda86a2be429fe4530b7
Terer Mike <terermike3@gmail.com>
2022-07-04 04:12:26 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
-
-int main()
+/**
+* main -  main function
+*
+* Return: returns 0 if the program runs successfully
+*/
+int main(void)
 {
 	int digit_1;
 	int digit_2;
 
 	for (digit_1 = 0; digit_1 <= 8; digit_1++)
 		{
-	        for (digit_2 = digit_1 + 1; digit_2 <= 9; digit_2++)
-		{
-		putchar((digit_1 % 10) + '0' );
-		putchar((digit_2 % 10) + '0');
-			if (digit_1 == 8 && digit_2 == 9)
-				continue;
-		putchar (',');
-		putchar (' ');
+	        	for (digit_2 = digit_1 + 1; digit_2 <= 9; digit_2++)
+			{
+			putchar((digit_1 % 10) + '0');
+			putchar((digit_2 % 10) + '0');
+				if (digit_1 == 8 && digit_2 == 9)
+					continue;
+			putchar (',');
+			putchar (' ');
+			}
 		}
-		}
-		putchar('\n');
-		return 0;
+	putchar('\n');
+	return (0);
 }
