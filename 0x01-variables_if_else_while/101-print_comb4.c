4a1491d63a297b11e0443374708411125c718716
Terer Mike <terermike3@gmail.com>
2022-07-04 08:51:12 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,7 +10,7 @@ int main(void)
 	int digit_1;
 	int digit_2;
 	int digit_3;
-	
+
 	for (digit_1 = 0; digit_1 <= 9; digit_1++)
 	{
 		for (digit_2 = digit_1 + 1; digit_2 <= 9; digit_2++)
@@ -20,7 +20,7 @@ int main(void)
 			putchar((digit_1 % 10) + '0');
 			putchar((digit_2 % 10) + '0');
 			putchar((digit_3 % 10) + '0');
-				if ( digit_1 == 7 && digit_2 == 8 && digit_3 ==9)
+				if (digit_1 == 7 && digit_2 == 8 && digit_3 == 9)
 					continue;
 				putchar(',');
 				putchar(' ');
