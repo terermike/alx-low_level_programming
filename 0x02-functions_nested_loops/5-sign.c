85485b5c9784a6291ebee1450cfa406faf04e68a
Terer Mike <terermike3@gmail.com>
2022-07-05 13:34:59 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,14 +13,14 @@ int print_sign(int n)
 	_putchar(43);
 	return (1);
 	}
-	else if (n == 0)
-	{
-	_putchar(0);
-	return (0);
-	}
-	else
+	else if (n < 0)
 	{
 	_putchar(45);
 	return (-1);
+	}
+	else
+	{
+	_putchar(0);
+	return (0);
 	}
 }
