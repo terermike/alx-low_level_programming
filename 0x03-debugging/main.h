f07d93c78a67d1f5521a4ebd9c8dab0d065ecb6c
Terer Mike <terermike3@gmail.com>
2022-07-06 09:43:51 -0700

    2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-positive_or_negative(int i);
-largest = largest_number(a, b, c);
+void positive_or_negative(int i);
+int largest_number(a, b, c);
 
 #endif /* MAIN_H */
