63f3d2fe952ccde28deceddc0727d4ba8de137e8
Terer Mike <terermike3@gmail.com>
2022-07-20 08:19:02 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,14 @@
 #include "main.h"
-
 /**
- * _puts_recursion - Print a string
+ * _print_rev_recursion - print a string in reverse
  * @s: string
  */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-		_putchar('\n');
-	else
-	{
-		_putchar(*s);
-		_puts_recursion(++s);
-	}
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
