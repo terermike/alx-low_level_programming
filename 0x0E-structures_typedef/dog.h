abb42bf2abb9143e7dc77ee1eda9e1d1a7ee7ac7
Terer Mike <terermike3@gmail.com>
2022-08-01 13:42:24 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @owner: owner's name
  * Description: new struct containing a dog's details
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /* DOG_H */
