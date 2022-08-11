ebffc46a6bbd28d4bfd677f901df92894caadc30
Terer Mike <terermike3@gmail.com>
2022-08-11 07:56:20 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,8 +14,8 @@ size_t print_list(const list_t *h)
 	while (current != NULL)
 	{
 	printf("[%d] %s\n", current->len, current->str);
-	count++;
 	current = current->next;
+	count++;
 	}
 	return (count);
 
