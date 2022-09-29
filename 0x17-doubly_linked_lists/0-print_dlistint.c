c0adcd861b4343ffeebbf2ba658211fc009f39ed
Terer <terermike3@gmail.com>
2022-09-29 04:00:44 -0700

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,6 +8,9 @@ size_t print_dlistint(const dlistint_t *h)
 {
     const dlistint_t *ptr = h;
     size_t n = 0;
+    
+    if (!h)
+        return (n);
 
     while (ptr && ptr->prev)
         ptr = ptr->prev;
