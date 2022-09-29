b6d9169ae497f7bc27ba690adfc3c0a479d6e7f5
Terer <terermike3@gmail.com>
2022-09-29 05:15:43 -0700

    2-add_dnodeint.c

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * add_dnodeint - function that adds a new node at the beginning of a dlistint
+ * _t list.
+ * @head: head of a doubly linked list.
+ * @n: data of type int to store in the list.
+ * Return: the address of the new element, or NULL if it failed.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+    dlistint_t *new;
+    
+    new = malloc(sizeof(dlistint_t));
+    if (!new)
+        return (NULL);
+    new->n = n;
+    new->next = *head;
+    new->prev = NULL;
+    *head = new;
+    return (*head);
+}
