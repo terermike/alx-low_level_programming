b1698bd7e8b862d88168ecc0353e764a1d7a6b71
Terer <terermike3@gmail.com>
2022-09-29 17:25:50 -0700

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,43 +1,54 @@
 #include "lists.h"
+
 /**
- * insert_dnodeint_at_index - function that inserts a new node at a given 
- * position.
- * @h: pointerto the doubly linked list.
- * @idx: position to insert node.
- * @n: data to store at node.
- * Return:  address of the new node, or NULL if it failed
- */ 
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
+ *
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t  *new, *next, *ptr = *h;
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
-		return (NULL);
-	new->n = n;
-	if (!h && idx == 0)
-	{
-	new->next = NULL;
-	new->prev = NULL;
-	return (new);
-	}
+	new = NULL;
+	if (idx == 0)
+		new = add_dnodeint(h, n);
 	else
 	{
-	while (ptr->prev)
-		ptr = ptr->prev;
-	while (idx - 1 > 0 && ptr)
-		ptr = ptr->next;
-	next = ptr->next;
-	ptr->next = new;
-	new->next = next;
-	new->prev = ptr;
-	if (!ptr->next)
-	{
-	new->next = NULL;
-	new->prev = ptr;
-	ptr->next = new;
-	return (new);
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
 	}
-	}
+
 	return (new);
 }
