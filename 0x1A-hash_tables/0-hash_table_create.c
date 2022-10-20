4315c12b5b1c23c8fd0aa050e05cfe6ddca163ce
Terer <terermike3@gmail.com>
2022-10-20 15:56:03 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 	unsigned long int i;
 
-	table = malloc(sizeof(hash_table_t));
+	if (size == 0)
+		return (NULL);
+	
+    table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
 	return (NULL);
