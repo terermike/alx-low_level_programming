9e92e69a26ddcd4fdd059f9963f06388fd7c0d03
Terer <terermike3@gmail.com>
2022-10-21 03:32:34 -0700

    1-djb2.c

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 -  implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ * Return: hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+    unsigned long int hash = 5381;
+    int i;
+
+    while ((i = *str++))
+    {     
+        hash = ((hash << 5) + hash) + i; /* hash * 33 + count */
+        i++;
+    }
+    return (hash);
+}
